b010446daffd0ca560c541bc7eb54fa98788dc7c
malin jonsson <malin--jonsson@hotmail.com>
2023-09-11 09:54:45 +0200

    title and btn

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+	return (
+		<>
+			<div className='container'>
+				<h1 className='title'>Ladda upp bröllopsbilder</h1>
+				<button className='btn'>Ladda upp filer</button>
+			</div>
+		</>
+	);
+}
+
+export default App;
