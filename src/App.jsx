c773e0386a03ee56d4fecfcfae72acde4e41a692
malin jonsson <malin--jonsson@hotmail.com>
2023-09-12 17:38:11 +0200

    btn download, show pic from bucket

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import AWS from 'aws-sdk';
 AWS.config.update({
 	accessKeyId: '',
 	secretAccessKey: '',
-	region: 'eu-north-1',
+	region: '',
   });
 
 const s3 = new AWS.S3();
 
 const App = () => {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [images, setImages] = useState([]);
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -38,15 +39,45 @@ const App = () => {
     });
   };
 
+  const getImages = () => {
+    const params = {
+      Bucket: 'fabulous-dolphins-uploads',
+    };
+
+    s3.listObjectsV2(params, (err, data) => {
+      if (err) {
+        console.error('Fel vid hämtning av bilder:', err);
+      } else {
+      
+        const imageList = data.Contents.map((obj) => obj.Key);
+        setImages(imageList);
+      }
+    });
+  };
+
   return (
     <>
       <div className='container'>
         <h1 className='title'>Ladda upp bröllopsbilder</h1>
         <input type="file" onChange={handleFileChange} />
         <button className='btn' onClick={handleUpload}>Ladda upp filer</button>
+
+        <button className='btn' onClick={getImages}>Visa befintliga bilder</button>
+        
+        {/* Visa befintliga bilder */}
+        <div className="image-list">
+          {images.map((imageName, index) => (
+            <img
+              key={index}
+              src={`https://fabulous-dolphins-uploads.s3.eu-north-1.amazonaws.com/${imageName}`}
+              alt={imageName}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
 };
+
 
 export default App;
--- src/lambda.js
+++ src/lambda.js
@@ -29,3 +29,38 @@
 //     body:JSON.stringify({error: 'Failed to get ContentType'})}
 // 	}
 // };
+
+
+
+
+
+
+
+// Therese lambda ...                                                         import { S3Client, HeadObjectCommand } from "@aws-sdk/client-s3"; 
+// export const handler = async (event) => {
+ 
+//     const bucketName = event.Records[0].s3.bucket.name;
+//     console.log("HÄR ÄR VÅRT EVENT!", JSON.stringify(event));
+//     const keyName = event.Records[0].s3.object.key;
+    
+//     const s3 = new S3Client({region: 'eu-north-1'});
+//     try {
+
+//         const params = {
+//             Bucket: bucketName,
+//             Key: keyName
+//         };
+
+//         const { ContentType } = await s3.send(new HeadObjectCommand(params)); 
+//         return {
+//             statusCode: 200,
+//             body: JSON.stringify({ contentType: ContentType })
+//         }
+//     } catch (err) {
+//         console.log('error!!!!', err);
+//         return {
+//             statusCode: 500,
+//             body: JSON.stringify({error: 'Failed to get content type'})
+//         }
+//     }
+// }
